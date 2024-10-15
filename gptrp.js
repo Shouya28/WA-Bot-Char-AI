949db40af2c8677fae160cb1fccb689701d84c1c
Ryzxell <161612079+Shouya28@users.noreply.github.com>
2024-10-15 19:08:14 +0700

    Update gptrp.js

--- gptrp.js
+++ gptrp.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const CHAR_ID = 'FRdKHnLG4JF6I14PexPWFvVyfrp-nZi6NFcoljmPPPM';
 const CHAT_ID = 'b1b68569-031d-4a64-a8ad-0878cae0fd59';
-const API_KEY = 'YOUR_APIKEY';
+const API_KEY = 'maelyn';
 const BASE_URL = 'https://api.maelyn.my.id/api/cai-chat';
 
 async function MealynAPI(query) {
